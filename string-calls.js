893e137c8da0f64e516ebaecf22a4d64643679f5
Timo J. Rinne <tri@iki.fi>
2024-10-22 19:02:22 +0300

    to_string() to string-calls.

--- string-calls.js
+++ string-calls.js
@@ -22,8 +22,40 @@ function strlen(...av) {
 	return av[0].length;
 }
 
+function to_string(...av) {
+	if ((av.length != 1) && (av.length != 2)) {
+		return null;
+	}
+	let x = av[0];
+
+	if (x === null) {
+		return 'NULL';
+	}
+	if (x === true) {
+		return 'TRUE';
+	}
+	if (x === false) {
+		return 'FALSE';
+	}
+	if (Number.isFinite(x)) {
+		let p = 16;
+		if (av.length == 2) {
+			if (Number.isSafeInteger(av[1]) && (av[1] >= 1) && (av[1] <= 100)) {
+				p = av[1];
+			} else if (! ((av[1] === null) || (av[1] === undefined))) {
+				return null;
+			}
+		}
+		return x.toPrecision(p);
+	}
+	if (typeof(x) === 'string') {
+		return x;
+	}
+	return null;
+}
+
 {
-	let ex = { strcat, strjoin, strlen };
+	let ex = { strcat, strjoin, strlen, to_string };
 	if (typeof(module) === 'object') {
 		module.exports = ex;
 	}
